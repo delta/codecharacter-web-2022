6c42180e5061d93482617aa2c9a46896c9a6a50e
Akash Jaiswal <akashjaiswal3846@gmail.com>
2022-01-26 12:46:51 +0530

    add: leaderboard

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.css';
 import styled from 'styled-components';
 
 const Body = styled.body`
@@ -8,34 +9,63 @@ const Body = styled.body`
   background: #242a3c;
 `;
 
+const Title = styled.h1`
+  color: white;
+  text-decoration: underline;
+  text-align: center;
+`;
+
+function createData(
+  rank: number,
+  username: string,
+  rating: number,
+  won: number,
+  lost: number,
+  tied: number,
+  fight: string,
+) {
+  return { rank, username, rating, won, lost, tied, fight };
+}
+
+const rows = [
+  createData(1, 'Akash1', 122, 6, 1, 1, 'fightakash'),
+  createData(2, 'Akash2', 122, 5, 1, 1, 'fightakash'),
+  createData(3, 'Akash3', 122, 4, 1, 1, 'fightakash'),
+  createData(4, 'Akash4', 122, 3, 1, 1, 'fightakash'),
+  createData(5, 'Akash5', 122, 2, 1, 1, 'fightakash'),
+  createData(6, 'Akash6', 122, 1, 1, 1, 'fightakash'),
+];
+
 export default function Leaderboard(): JSX.Element {
   return (
     <Body>
-      <Team name="Hawks" />
-      <Team name="Eagles" />
+      <Title>Leaderboard</Title>
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Username</th>
+            <th>Rating</th>
+            <th>Won</th>
+            <th>Tied</th>
+            <th>Lost</th>
+            <th>Play Against</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map(row => (
+            <tr key={row.username}>
+              <td align="center">{row.rank}</td>
+              <td align="center">{row.username}</td>
+              <td align="center">{row.rating}</td>
+              <td align="center">{row.won}</td>
+              <td align="center">{row.lost}</td>
+              <td align="center">{row.tied}</td>
+              <button>{row.fight}</button>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </Body>
   );
-}
-
-class Team extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { score: 0 };
-  }
-
-  increaseScore() {
-    this.setState({
-      score: this.state.score + 2,
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h2>{this.props.name}</h2>
-        <h1>{this.state.score}</h1>
-        <button onClick={this.increaseScore}>+2</button>
-      </div>
-    );
-  }
 }
