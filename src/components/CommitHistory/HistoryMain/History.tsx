c1ffb1641b7c315bd0c0af97c342a6419c08afc4
Dhanush-T <dhanushdan26@gmail.com>
2022-01-28 21:03:48 +0530

    add: History page with commit tree component added

---
+++ src/components/CommitHistory/HistoryMain/History.tsx
@@ -0,0 +1,54 @@
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import CommitHistory from '../CommitTree/CommitHistroy';
+import './History.css';
+
+export default function History(): JSX.Element {
+  const [BigButton, setBigButton] = useState('Code');
+
+  return (
+    <div>
+      <div className="HistoryMain">
+        <div className="timeline">
+          <div></div>
+          <div className="completeTimeline">
+            <CommitHistory />
+          </div>
+          <div></div>
+        </div>
+        <div className="codeview">
+          <div className="Code-Match">
+            <ButtonGroup>
+              <Button
+                className={BigButton == 'Code' ? 'LargeButton' : 'SmallButton'}
+                onClick={() => {
+                  setBigButton('Code');
+                }}
+              >
+                Code
+              </Button>
+              <Button
+                className={BigButton == 'Map' ? 'LargeButton' : 'SmallButton'}
+                onClick={() => {
+                  setBigButton('Map');
+                }}
+              >
+                Map
+              </Button>
+            </ButtonGroup>
+          </div>
+          <div className="CodeBox">
+            <h1 style={{ color: 'white' }}>Hello World</h1>
+          </div>
+          <div className="Select">
+            <Button className="SelectButton" variant="primary" size="lg">
+              Select
+            </Button>
+          </div>
+        </div>
+        <div className="dummy"></div>
+      </div>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 
 const Home = lazy(() => import('./pages/Home'));
 const Dashboard = lazy(() => import('./pages/Dashboard'));
+const History = lazy(
+  () => import('./components/CommitHistory/HistoryMain/History'),
+);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,6 +15,7 @@ ReactDOM.render(
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/history" element={<History />} />
         </Routes>
       </Suspense>
     </HashRouter>
