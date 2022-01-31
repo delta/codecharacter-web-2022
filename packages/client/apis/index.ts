643a344d8f140748442508de01b9058a661a3ecc
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2022-02-01 01:54:39 +0530

    feat: Add API client

---
+++ packages/client/apis/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthApi';
+export * from './CodeApi';
+export * from './CurrentUserApi';
+export * from './GameApi';
+export * from './LeaderboardApi';
+export * from './MapApi';
+export * from './MatchApi';
+export * from './NotificationApi';
+export * from './UserApi';
