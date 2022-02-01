45430da027ab2f17c0b3474c90c08b61d08ea8ab
Ram-20062003 <ramajs2003@gmail.com>
2022-02-01 12:17:42 +0530

    remove: deleted config.ts

--- src/config/config.ts
+++
@@ -1,3 +0,0 @@
-export const SITE_KEY = '6LdYAzgeAAAAAFvfmHT_tvBg1sbcj7OG7epzxfOQ';
-export const SECRET_KEY = '6LdYAzgeAAAAAOhjsWoAnmjRyB0JOiZCJRbYANOy';
-export const accessUrl = 'ACCESS URL FOR CORS';
