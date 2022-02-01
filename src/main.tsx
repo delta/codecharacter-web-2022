0b9f103f9d3cca5b3a1f54d51d47d92aa8caa9db
Ram Ganesh K. R <83528327+Ram-20062003@users.noreply.github.com>
2022-02-01 21:32:21 +0530

    feat: Add login, register page and user profile (#8)
    * add: Login and register pages
    
    * feat: user profile added
    
    * fix: type fix for grecaptcha error in register page
    
    * feat: routes for login and register page
    
    * necessary packages are added and modified config to add style module
    
    * remove: removed logs and secret keys
    
    * fix: UI in register page and user proile , moved profile styles to components
    
    * fix: moved secrets to config, code formatted
    
    * Update CI to copy config
    
    * fix: changed a tag to link
    
    * refact: separate functions are made
    
    * refactor: styles
    
    * fix: className in auth.module.css
    
    * fix: class specificty in auth.module.css
    
    * fix: inline styles are changed to classnames
    
    * refact: Modified to component structured
    
    * fix(UI): Font added and other minor changes
    
    * remove: deleted config.ts
    
    * fix: added config to gitignore
    
    * fix: lint
    
    Co-authored-by: Rinish Sam <36656347+CaptainIRS@users.noreply.github.com>
    Co-authored-by: Priyanshu Dangi <priyanshudangi37@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import NavBar from './components/NavBar/NavBar';
 import SideBar from './components/SideBar/SideBar';
 import Toast from './components/Toast/Toast';
@@ -10,6 +12,8 @@ const Dashboard = lazy(() => import('./pages/Dashboard'));
 const History = lazy(
   () => import('./components/CommitHistory/HistoryMain/History'),
 );
+const Login = lazy(() => import('./pages/Auth/Login'));
+const Register = lazy(() => import('./pages/Auth/Register'));
 
 ReactDOM.render(
   <React.StrictMode>
@@ -23,6 +27,8 @@ ReactDOM.render(
               <Route path="/" element={<Home />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/history" element={<History />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
             </Routes>
           </Suspense>
         </div>
