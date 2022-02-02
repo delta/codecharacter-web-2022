2ebfae37cc420b9cc72aa650fb2251789585ccdc
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2022-02-02 22:19:36 +0530

    feat: Add API middleware

--- src/config/config.example.ts
+++ src/config/config.example.ts
@@ -1,3 +1,5 @@
+export const BASE_PATH = 'http://localhost:8080';
+export const PREFER_DEV_OVERRIDE = '';
 export const SITE_KEY = 'YOUR SITE KEY';
 export const SECRET_KEY = 'YOUR SECRET KEY';
 export const accessUrl = 'ACCESS URL FOR CORS';
