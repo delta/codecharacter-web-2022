2ec6dbb8ba9bc71f6fb68a798192d00bb6d0347a
Rinish Sam <36656347+CaptainIRS@users.noreply.github.com>
2022-02-02 22:33:44 +0530

    feat: Add API middleware (#17)

---
+++ src/api/ApiConfig.ts
@@ -0,0 +1,30 @@
+import { Configuration } from '@codecharacter-2022/client';
+import { BASE_PATH, PREFER_DEV_OVERRIDE } from '../config/config';
+
+export class ApiError extends Error {
+  status: number;
+  message: string;
+
+  constructor(status: number, message: string) {
+    super(message);
+    this.status = status;
+    this.message = message;
+  }
+}
+
+export const apiConfig = new Configuration({
+  basePath: BASE_PATH,
+  headers: PREFER_DEV_OVERRIDE ? { Prefer: PREFER_DEV_OVERRIDE } : {},
+  middleware: [
+    {
+      post: async context => {
+        const status = context.response.status;
+        if (status >= 400) {
+          const body = await context.response.json();
+          throw new ApiError(status, body?.message ?? 'Unknown error');
+        }
+        return context.response;
+      },
+    },
+  ],
+});
