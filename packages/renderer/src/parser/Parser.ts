5583f71fca6609290000f2f6b9bc9bddfb6aadfb
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2022-02-04 00:39:49 +0530

    Refactor log parser

--- packages/renderer/src/parser/Parser.ts
+++ packages/renderer/src/parser/Parser.ts
@@ -1,14 +1,14 @@
-import * as Instr from './instruction.js';
+import * as instructions from './Instructions.js';
 
-export function Parse(line: String): Instr.Instruction {
+export function Parse(line: string): instructions.Instruction {
   const ParsedArray = line.split(', ');
 
   if (ParsedArray.length === 0) {
-    return new Instr.InvalidInst();
+    return new instructions.InvalidInst();
   }
 
   if (ParsedArray[0] === 'INIT') {
-    return new Instr.Init();
+    return new instructions.Init();
   }
 
   if (ParsedArray[0] === 'TOWERS') {
@@ -17,7 +17,7 @@ export function Parse(line: String): Instr.Instruction {
     if (EachTowerDetails) {
       const AllTowers = EachTowerDetails.map(tower => {
         const TowerInfo = tower.replace(/[()]/g, '').split(', ');
-        return new Instr.Tower(
+        return new instructions.Tower(
           Number(TowerInfo[0]),
           Number(TowerInfo[1]),
           Number(TowerInfo[2]),
@@ -25,16 +25,16 @@ export function Parse(line: String): Instr.Instruction {
           Number(TowerInfo[4]),
         );
       });
-      return new Instr.Towers(AllTowers);
+      return new instructions.Towers(AllTowers);
     }
   }
 
   if (ParsedArray[0] === 'TURN') {
-    return new Instr.Turn(Number(ParsedArray[1]));
+    return new instructions.Turn(Number(ParsedArray[1]));
   }
 
   if (ParsedArray[0] === 'MOVE') {
-    return new Instr.Move(
+    return new instructions.Move(
       Number(ParsedArray[1]),
       Number(ParsedArray[2]),
       Number(ParsedArray[3]),
@@ -42,15 +42,16 @@ export function Parse(line: String): Instr.Instruction {
   }
 
   if (ParsedArray[0] === 'SHOOT') {
-    return new Instr.Shoot(
-      Number(ParsedArray[1]),
+    return new instructions.Shoot(
+      String(ParsedArray[1]),
       Number(ParsedArray[2]),
       Number(ParsedArray[3]),
+      Number(ParsedArray[4]),
     );
   }
 
   if (ParsedArray[0] === 'SPAWN') {
-    return new Instr.Spawn(
+    return new instructions.Spawn(
       Number(ParsedArray[1]),
       Number(ParsedArray[2]),
       Number(ParsedArray[3]),
@@ -60,28 +61,33 @@ export function Parse(line: String): Instr.Instruction {
   }
 
   if (ParsedArray[0] === 'DEAD') {
-    return new Instr.Dead(Number(ParsedArray[1]));
+    return new instructions.Dead(
+      String(ParsedArray[1]),
+      Number(ParsedArray[2]),
+    );
   }
 
   if (ParsedArray[0] === 'PRINT') {
-    return new Instr.Print(ParsedArray[1]);
+    return new instructions.Print(ParsedArray[1]);
   }
 
   if (ParsedArray[0] === 'DESTRUCTION') {
-    return new Instr.Destruction(Number(ParsedArray[1].replace('%', '')));
+    return new instructions.Destruction(
+      Number(ParsedArray[1].replace('%', '')),
+    );
   }
 
   if (ParsedArray[0] === 'COINS') {
-    return new Instr.Coins(Number(ParsedArray[1]));
+    return new instructions.Coins(Number(ParsedArray[1]));
   }
 
   if (ParsedArray[0] === 'ERRORS') {
-    return new Instr.Errors(ParsedArray[1]);
+    return new instructions.Errors(ParsedArray[1]);
   }
 
   if (ParsedArray[0] === 'END') {
-    return new Instr.End();
+    return new instructions.End();
   }
 
-  return new Instr.InvalidInst();
+  return new instructions.InvalidInst();
 }
