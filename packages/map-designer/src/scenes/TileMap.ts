4d3b82ea64b774ed1293b1ffa6142bb8db07bae8
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2022-02-04 20:09:14 +0530

    Fix coins used on load

--- packages/map-designer/src/scenes/TileMap.ts
+++ packages/map-designer/src/scenes/TileMap.ts
@@ -348,6 +348,7 @@ export class TileMap extends Phaser.Scene {
       }
     }
 
+    this.coinsUsed = coins;
     events.emit(MapDesignerEvents.COINS_CHANGED, Parameters.totalCoins - coins);
   }
 
