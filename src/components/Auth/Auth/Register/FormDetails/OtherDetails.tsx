37b1a35ec08917553c3e23ebc9fde23b5f8b01d9
Ram-20062003 <ramajs2003@gmail.com>
2022-02-07 05:09:28 +0530

    refact: moved subcomponents to spearate folder

---
+++ src/components/Auth/Auth/Register/FormDetails/OtherDetails.tsx
@@ -0,0 +1,120 @@
+import { Form } from 'react-bootstrap';
+import ReactFlagsSelect from 'react-flags-select';
+import AlertMessage from '../../Alert/Alert';
+import styles from './OtherDetails.module.css';
+
+const avatar = [
+  {
+    url: '1',
+  },
+  {
+    url: '2',
+  },
+  {
+    url: '3',
+  },
+  {
+    url: '4',
+  },
+  {
+    url: '5',
+  },
+  {
+    url: '6',
+  },
+  {
+    url: '5',
+  },
+  {
+    url: '6',
+  },
+  {
+    url: '5',
+  },
+  {
+    url: '6',
+  },
+  {
+    url: '5',
+  },
+  {
+    url: '6',
+  },
+  {
+    url: '6',
+  },
+  {
+    url: '5',
+  },
+  {
+    url: '6',
+  },
+];
+
+export default function OtherDetails(props: user): JSX.Element {
+  return (
+    <div>
+      <div className={styles.levelTitle}>Other Details</div>
+      <Form.Group className="mb-3" controlId="formBasicCountryName">
+        <Form.Label>Country</Form.Label>
+        <div className={styles.flagContainer}>
+          <ReactFlagsSelect
+            searchable
+            selected={props.selectedCode != null ? props.selectedCode : 'IN'}
+            onSelect={
+              props.handleFlagSelect != null
+                ? props.handleFlagSelect
+                : () => {
+                    return;
+                  }
+            }
+          />
+        </div>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCollegename">
+        <Form.Label>College</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="College"
+          value={props.college}
+          onChange={props.handleCollegeChange}
+          className={
+            props.formNumber === 3 && props.completed
+              ? props.collegeError
+                ? styles.error
+                : styles.correct
+              : styles.normal
+          }
+        />
+        {props.formNumber == 3 && props.collegeError ? (
+          <AlertMessage
+            err={props.collegeError}
+            content={'Please Enter your College Name'}
+          />
+        ) : (
+          <></>
+        )}
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicAvatar">
+        <Form.Label>Avatar</Form.Label>
+        <div className={styles.avatarBox}>
+          <div className={styles.avatarContainer}>
+            {avatar.map((avatar, index: number) => (
+              <div key={index} className={styles.avatar}></div>
+            ))}
+          </div>
+        </div>
+      </Form.Group>
+    </div>
+  );
+}
+
+interface user {
+  selectedCode?: string;
+  handleFlagSelect?: (code: string) => void;
+  formNumber?: number;
+  handleCollegeChange?: React.ChangeEventHandler<HTMLInputElement>;
+  college?: string;
+  collegeError?: boolean;
+  completed?: boolean;
+}
---
+++ src/components/Auth/Auth/Register/FormDetails/UserCreditionals.tsx
@@ -0,0 +1,69 @@
+import { Form } from 'react-bootstrap';
+import AlertMessage from '../../Alert/Alert';
+import styles from './user.module.css';
+
+export default function UserCreditionals(props: creditionals): JSX.Element {
+  return (
+    <div>
+      <div className={styles.levelTitle}>User Creditionals</div>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          value={props.password}
+          onChange={props.handlePasswordChange}
+          className={
+            props.submitSecond
+              ? props.passwordError
+                ? styles.error
+                : styles.correct
+              : styles.normal
+          }
+        />
+        {props.submitSecond && props.passwordError ? (
+          <AlertMessage
+            err={props.passwordError}
+            content={'Password should be atleast 5 characters'}
+          />
+        ) : (
+          <></>
+        )}
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+        <Form.Label>Confirm password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Confirm Password"
+          value={props.confirmPassword}
+          onChange={props.handleConfirmPasswordChange}
+          className={
+            props.submitSecond
+              ? props.confirmpasswordError
+                ? styles.error
+                : styles.correct
+              : styles.normal
+          }
+        />
+        {props.submitSecond && props.confirmpasswordError ? (
+          <AlertMessage
+            err={props.confirmpasswordError}
+            content={'Please! check your password'}
+          />
+        ) : (
+          <></>
+        )}
+      </Form.Group>
+    </div>
+  );
+}
+
+interface creditionals {
+  submitSecond?: boolean;
+  handlePasswordChange?: React.ChangeEventHandler<HTMLInputElement>;
+  handleConfirmPasswordChange?: React.ChangeEventHandler<HTMLInputElement>;
+  password?: string;
+  passwordError?: boolean;
+  confirmPassword?: string;
+  confirmpasswordError?: boolean;
+}
