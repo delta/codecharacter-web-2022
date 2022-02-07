506b25af305ad616a07df91481f36bae6637084a
Priyanshu Dangi <priyanshudangi37@gmail.com>
2022-02-08 00:20:53 +0530

    feat: Add redux initial setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 
 import NavBar from './components/NavBar/NavBar';
 import SideBar from './components/SideBar/SideBar';
 import Toast from './components/Toast/Toast';
 import styles from './main.module.css';
+import { Counter } from './store/counter/Counter';
 const Home = lazy(() => import('./pages/Home'));
 const Dashboard = lazy(() => import('./pages/Dashboard'));
 const History = lazy(
@@ -17,6 +20,7 @@ const Register = lazy(() => import('./pages/Auth/Register'));
 
 ReactDOM.render(
   <React.StrictMode>
+  <Provider store={store}>
     <HashRouter>
       <NavBar />
       <div className={styles.mainWindow}>
@@ -29,12 +33,14 @@ ReactDOM.render(
               <Route path="/history" element={<History />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
+              <Route path="/counter" element={<Counter />} />
             </Routes>
           </Suspense>
         </div>
       </div>
     </HashRouter>
     <Toast />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
