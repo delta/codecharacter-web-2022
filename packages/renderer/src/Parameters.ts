442440db7d636569db66185d3d037e1b8b6fef63
Rinish Sam <36656347+CaptainIRS@users.noreply.github.com>
2022-02-10 07:38:15 +0530

    feat: Add renderer (#18)
    * feat: Add renderer
    
    * fix: Handle shutdown and rename healthbar
    
    * Fix pause/resume logic
    
    * Fix depth sorting
    
    * fix: reset logic

---
+++ packages/renderer/src/Parameters.ts
@@ -0,0 +1,19 @@
+export class Parameters {
+  static timePerTurn = 1000;
+
+  static mapWidth = 64;
+
+  static mapHeight = 64;
+
+  static gridCellWidth = 128;
+
+  static gridCellHeight = 64;
+
+  static mapTileWidth = 512;
+
+  static mapTileHeight = 512;
+
+  static mapTileHalfWidth = 256;
+
+  static mapTileHalfHeight = 256;
+}
