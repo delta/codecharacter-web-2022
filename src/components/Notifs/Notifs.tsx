fc7034538e2144cd424b29a30f0a08ea9b359e7f
Vignesh Duraisamy <vigneshd332@gmail.com>
2022-02-12 03:06:26 +0530

    feat: Add notif read marking

--- src/components/Notifs/Notifs.tsx
+++ src/components/Notifs/Notifs.tsx
@@ -6,13 +6,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell } from '@fortawesome/free-solid-svg-icons';
 import {
   getNotifAction,
+  markNotifAction,
   notifs as notifications,
 } from '../../store/Notifs/NotifSlice';
 
 const Notifs: React.FunctionComponent = () => {
   const notifModalRef = useRef<HTMLDivElement>(null);
   const notifIconRef = useRef<HTMLDivElement>(null);
-  const notifs = useAppSelector(notifications);
+  const notifs = useAppSelector<Notification[]>(notifications);
   const hookDispatch = useAppDispatch();
 
   useEffect(() => {
@@ -20,9 +21,26 @@ const Notifs: React.FunctionComponent = () => {
   }, []);
 
   const toggleNotifModal = () => {
+    if (!notifModalRef.current?.classList.contains(styles.notifModalShow)) {
+      hookDispatch(markNotifAction(notifs[notifs.length - 1].id));
+    }
     notifModalRef.current?.classList.toggle(styles.notifModalShow);
     notifIconRef.current?.classList.toggle(styles.notifIconShow);
   };
+
+  const parseDate = (date: Date) => {
+    const createdDate = new Date(date);
+    const diff =
+      (new Date().getTime() - createdDate.getTime()) / (1000 * 3600 * 24);
+    const dateDiff =
+      diff === 0
+        ? `Today`
+        : diff === 1
+        ? `Yesterday`
+        : `${Math.round(diff)} days ago`;
+    return `${createdDate.getHours()}:${createdDate.getMinutes()}, ${dateDiff}`;
+  };
+
   return (
     <>
       <div
@@ -43,7 +61,7 @@ const Notifs: React.FunctionComponent = () => {
             >
               <div className={styles.notifLine}>
                 <h5 className={styles.notifHeader}>{notif.title}</h5>
-                <p className={styles.notifTime}>{notif.createdAt}</p>
+                <p className={styles.notifTime}>{parseDate(notif.createdAt)}</p>
               </div>
               <div className={styles.notifLine}>
                 <p className={styles.notifContent}>{notif.content}</p>
