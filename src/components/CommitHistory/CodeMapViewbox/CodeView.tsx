5ff9af137e0b704920e3ede9b8e47485d6f9cd12
Dhanush T <dhanushdan26@gmail.com>
2022-02-18 01:23:57 +0530

    fix: imported only the required languages module

--- src/components/CommitHistory/CodeMapViewbox/CodeView.tsx
+++ src/components/CommitHistory/CodeMapViewbox/CodeView.tsx
@@ -1,6 +1,15 @@
 import { CodeRevision } from '@codecharacter-2022/client';
 import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import c from 'react-syntax-highlighter/dist/esm/languages/prism/c';
+import cpp from 'react-syntax-highlighter/dist/esm/languages/prism/cpp';
+import python from 'react-syntax-highlighter/dist/esm/languages/prism/python';
+import java from 'react-syntax-highlighter/dist/esm/languages/prism/java';
 import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+SyntaxHighlighter.registerLanguage('c', c);
+SyntaxHighlighter.registerLanguage('cpp', cpp);
+SyntaxHighlighter.registerLanguage('python', python);
+SyntaxHighlighter.registerLanguage('java', java);
 
 type PropsType = {
   code: CodeRevision['code'];
