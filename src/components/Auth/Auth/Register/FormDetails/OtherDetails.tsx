645a92d2fe6d11f29cc7adebfa64fd1aecff7412
Vignesh Duraisamy <vigneshd332@gmail.com>
2022-02-26 23:56:37 +0530

    refactor: Avatar is now a component

--- src/components/Auth/Auth/Register/FormDetails/OtherDetails.tsx
+++ src/components/Auth/Auth/Register/FormDetails/OtherDetails.tsx
@@ -3,113 +3,11 @@ import { Form } from 'react-bootstrap';
 import ReactFlagsSelect from 'react-flags-select';
 import AlertMessage from '../../Alert/Alert';
 import styles from './OtherDetails.module.css';
-
-import duck from '../../../../../assets/avatars/duck.png';
-import elephant from '../../../../../assets/avatars/elephant.png';
-import giraffe from '../../../../../assets/avatars/giraffe.png';
-import frog from '../../../../../assets/avatars/frog.png';
-import monkey from '../../../../../assets/avatars/monkey.png';
-import moose from '../../../../../assets/avatars/moose.png';
-import narwhal from '../../../../../assets/avatars/narwhal.png';
-import owl from '../../../../../assets/avatars/owl.png';
-import panda from '../../../../../assets/avatars/panda.png';
-import parrot from '../../../../../assets/avatars/parrot.png';
-import penguin from '../../../../../assets/avatars/penguin.png';
-import pig from '../../../../../assets/avatars/pig.png';
-import rabbit from '../../../../../assets/avatars/rabbit.png';
-import rhino from '../../../../../assets/avatars/rhino.png';
-import sloth from '../../../../../assets/avatars/sloth.png';
-import snake from '../../../../../assets/avatars/snake.png';
-import walrus from '../../../../../assets/avatars/walrus.png';
-import whale from '../../../../../assets/avatars/whale.png';
-import zebra from '../../../../../assets/avatars/zebra.png';
-import horse from '../../../../../assets/avatars/horse.png';
-
-const avatar = [
-  {
-    id: 0,
-    url: duck,
-  },
-  {
-    id: 1,
-    url: elephant,
-  },
-  {
-    id: 2,
-    url: giraffe,
-  },
-  {
-    id: 3,
-    url: frog,
-  },
-  {
-    id: 4,
-    url: monkey,
-  },
-  {
-    id: 5,
-    url: moose,
-  },
-  {
-    id: 6,
-    url: narwhal,
-  },
-  {
-    id: 7,
-    url: owl,
-  },
-  {
-    id: 8,
-    url: panda,
-  },
-  {
-    id: 9,
-    url: parrot,
-  },
-  {
-    id: 10,
-    url: penguin,
-  },
-  {
-    id: 11,
-    url: pig,
-  },
-  {
-    id: 12,
-    url: rabbit,
-  },
-  {
-    id: 13,
-    url: rhino,
-  },
-  {
-    id: 14,
-    url: sloth,
-  },
-  {
-    id: 15,
-    url: snake,
-  },
-  {
-    id: 16,
-    url: walrus,
-  },
-  {
-    id: 17,
-    url: whale,
-  },
-  {
-    id: 18,
-    url: zebra,
-  },
-  {
-    id: 19,
-    url: horse,
-  },
-];
+import { getAllAvatars } from '../../../../Avatar/Avatar';
 
 export default function OtherDetails(props: user): JSX.Element {
   const [selectedAvatar, setSelectedAvatar] = useState(0);
+  const avatars = getAllAvatars();
 
   return (
     <div>
@@ -162,7 +60,7 @@ export default function OtherDetails(props: user): JSX.Element {
         <Form.Label>Avatar</Form.Label>
         <div className={styles.avatarBox}>
           <div className={styles.avatarContainer}>
-            {avatar.map((avatar, index: number) => (
+            {avatars.map((avatar, index: number) => (
               <div
                 key={index}
                 className={`${styles.avatar} ${
