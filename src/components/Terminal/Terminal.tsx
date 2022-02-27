a013e83832b938cf11a8e4d2ebb5297bf4607510
Priyanshu Dangi <priyanshudangi37@gmail.com>
2022-02-27 23:31:52 +0530

    feat: auto scroll logger

--- src/components/Terminal/Terminal.tsx
+++ src/components/Terminal/Terminal.tsx
@@ -1,25 +1,44 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { RendererUtils } from '@codecharacter-2022/renderer';
 import ReactAnsi from 'react-ansi';
 
 export default function Terminal(): JSX.Element {
   const [log, setLog] = useState('');
+  const ref = useRef<HTMLDivElement>(null);
 
   RendererUtils.setUpdateLogCallback((log: string) => {
     setLog(log);
   });
 
+  useEffect(() => {
+    const element = document.querySelector('#log > div:nth-child(1) > div');
+    if (
+      element &&
+      element?.scrollTop + element?.clientHeight + 50 > element?.scrollHeight
+    ) {
+      element?.scrollTo(0, element.scrollHeight);
+    }
+  }, [log]);
+
   return (
-    <ReactAnsi
+    <div
+      ref={ref}
       style={{
         height: '100%',
         width: '100%',
       }}
-      log={log}
-      bodyStyle={{ height: '100%' }}
-      logStyle={{ height: '100%' }}
-      autoScroll={true}
-      virtual
-    />
+    >
+      <ReactAnsi
+        style={{
+          height: '100%',
+          width: '100%',
+        }}
+        log={log}
+        bodyStyle={{ height: '100%', overflowY: 'auto' }}
+        logStyle={{ height: '100%' }}
+        autoScroll={true}
+        virtual
+      />
+    </div>
   );
 }
