077119083e656d5853312f1c8a98d654d2a69f60
Vignesh Duraisamy <vigneshd332@gmail.com>
2022-02-27 16:59:55 +0530

    fix: Loader now covers full page

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,12 +1,14 @@
 import styles from './Loader.module.css';
 export default function Loader(): JSX.Element {
   return (
-    <div className={styles.container}>
-      <div className={styles.dot}></div>
-      <div className={styles.dot}></div>
-      <div className={styles.dot}></div>
-      <div className={styles.dot}></div>
-      <div className={styles.dot}></div>
+    <div className={styles.overlayBackground}>
+      <div className={styles.container}>
+        <div className={styles.dot}></div>
+        <div className={styles.dot}></div>
+        <div className={styles.dot}></div>
+        <div className={styles.dot}></div>
+        <div className={styles.dot}></div>
+      </div>
     </div>
   );
 }
