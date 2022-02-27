46049ee1951432529bdbaa450b0d83a154c82c82
Priyanshu Dangi <priyanshudangi37@gmail.com>
2022-02-27 17:28:46 +0530

    feat: protected and private routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './main.module.css';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Toast from './components/Toast/Toast';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
@@ -10,39 +10,16 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
 import Loader from './components/Loader/Loader';
 import './websocket/websocket';
+import AllRoutes from './AllRoutes';
 
 const persistor = persistStore(store);
-const Home = lazy(() => import('./pages/Home/Home'));
-const Dashboard = lazy(() => import('./pages/Dashboard/Dashboard'));
 const NavBar = lazy(() => import('./components/NavBar/NavBar'));
-const MapDesigner = lazy(() => import('./components/MapDesigner/MapDesigner'));
 const SideBar = lazy(() => import('./components/SideBar/SideBar'));
-const History = lazy(
-  () => import('./components/CommitHistory/HistoryMain/History'),
-);
-const Login = lazy(() => import('./pages/Auth/LoginForm'));
-const Register = lazy(() => import('./pages/Auth/RegisterForm'));
 const EditorSettings = lazy(
   () => import('./components/EditorSettings/EditorSettings'),
 );
-const Leaderboard = lazy(() => import('./components/Leaderboard/Leaderboard'));
-const BattleTV = lazy(() => import('./components/BattleTV/BattleTV'));
 const SelfMatchModal = lazy(
   () => import('./components/SelfMatchMakingModal/SelfMatchMakeModal'),
-);
-const Verify = lazy(
-  () => import('./components/Auth/Auth/Register/ActivateUser/ActivateUser'),
-);
-const ResetPassword = lazy(
-  () =>
-    import(
-      './components/Auth/Auth/Login/ForgetPassword/ResetpasswordVerifcation'
-    ),
-);
-
-const IncompleteProfile = lazy(
-  () =>
-    import('./components/Auth/Auth/Login/IncompleteProfile/incompeleteProfile'),
 );
 
 ReactDOM.render(
@@ -57,22 +34,7 @@ ReactDOM.render(
             <div className={styles.mainWindow}>
               <SideBar />
               <div className={styles.gameArea}>
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/dashboard" element={<Dashboard />} />
-                  <Route path="/mapdesigner" element={<MapDesigner />} />
-                  <Route path="/history" element={<History />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/register" element={<Register />} />
-                  <Route path="/leaderboard" element={<Leaderboard />} />
-                  <Route path="/battletv" element={<BattleTV />} />
-                  <Route path="/activate" element={<Verify />} />
-                  <Route path="/reset-password" element={<ResetPassword />} />
-                  <Route
-                    path="/incomplete-profile"
-                    element={<IncompleteProfile />}
-                  />
-                </Routes>
+                <AllRoutes />
               </div>
             </div>
           </Suspense>
