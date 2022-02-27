84c38aed3d27a5a57ff930ac048b0fae980e8aac
Akash Jaiswal <akashjaiswal3846@gmail.com>
2022-02-28 03:48:13 +0530

    feat: rows (details, about, screenshot)

--- src/components/Home/Row/Rows.tsx
+++ src/components/Home/Row/Rows.tsx
@@ -11,17 +11,18 @@ interface rowType {
 const rowData: Array<rowType> = [
   {
     image: image,
-    title: 'Build Your Army',
-    description: 'Build Your Army',
+    title: 'About',
+    description:
+      ' Code Character is an online AI programming competition, where you write C++ code for a real time strategy game. Test your code against yourself, against the computer, against your previous versions and then against everyone else on multiple maps!    Challenge others to improve your rating as you climb up the leaderboard! Integrated code editor and development environment View your AI play with different versions of itself and with existing AI Bots Live leaderboard, challenge anyone. Active discussion forum for any game related questions Extensive tutorials and documentation',
   },
   {
     image: image,
-    title: 'Build Your Army',
-    description: 'Build Your Army',
+    title: 'Details',
+    description: 'Date and prizes',
   },
   {
     image: image,
-    title: 'Build Your Army',
+    title: 'Screenshots',
     description: 'Build Your Army',
   },
 ];
