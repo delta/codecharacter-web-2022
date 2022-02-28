1a1d85719316f7816c5a38ef5bc348cf35afc199
Vignesh Duraisamy <vigneshd332@gmail.com>
2022-02-28 15:10:53 +0530

    fix: make glitch text responsive

--- src/components/Home/Glitchtext/Glitchtext.tsx
+++ src/components/Home/Glitchtext/Glitchtext.tsx
@@ -2,9 +2,11 @@ import styles from './Glitchtext.module.css';
 export default function Glitchtext(prop: { value: string }): JSX.Element {
   return (
     <div className={styles.container}>
-      <div className={styles.glitchtext}>{prop.value}</div>
-      <div className={styles.glitchtext}>{prop.value}</div>
-      <div className={styles.glitchtext}>{prop.value}</div>
+      <div className={styles.textContainer}>
+        <div className={styles.glitchtext}>{prop.value}</div>
+        <div className={styles.glitchtext}>{prop.value}</div>
+        <div className={styles.glitchtext}>{prop.value}</div>
+      </div>
     </div>
   );
 }
