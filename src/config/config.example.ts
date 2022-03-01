e4aef738142fe6c4df6f3519a48dd64b60232b82
Priyanshu Dangi <51911161+PriyanshuDangi@users.noreply.github.com>
2022-03-02 01:16:27 +0530

    fix: logout prod (#70)
    * fix: remove cookie
    
    * fix: remove cookie in prod

--- src/config/config.example.ts
+++ src/config/config.example.ts
@@ -4,3 +4,5 @@ export const SITE_KEY = 'YOUR SITE KEY';
 export const SECRET_KEY = 'YOUR SECRET KEY';
 export const accessUrl = 'ACCESS URL FOR CORS';
 export const homeUrl = 'http://localhost:3000';
+
+export const cookieDomain = 'localhost';
